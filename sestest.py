6466ece542359dd25c42348d6a9b250b79900987
Boyu Wang <boyu.wang@sony.com>
2017-11-27 03:42:50 +0000

    sestest

---
+++ sestest.py
@@ -0,0 +1,28 @@
+import boto3
+
+def lambda_handler(event, context):
+  sendEmail()
+
+def sendEmail():
+  ses = boto3.client('ses',region_name="us-west-2")
+  response = ses.send_email(
+            Source=os.environ['SES_SRC'],
+            Destination={
+                'ToAddresses': [
+                    'boyu.wang@sony.com',
+                ],
+            },
+            Message={
+                'Subject': {
+                    'Data': 'ses change src test',
+                },
+                'Body':{
+                    'Text': {
+                        'Charset': 'UTF-8',
+                        'Data': "nothing"
+                    }
+                }
+            }
+    )
+if __name__ == "__main__":
+  sendEmail()
