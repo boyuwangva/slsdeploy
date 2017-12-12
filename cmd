fc90ee07600a902df2388347a01d325fd3454918
Boyu Wang <boyu.wang@sony.com>
2017-12-12 05:29:27 +0000

    test

---
+++ cmd
@@ -0,0 +1,3 @@
+　deploy_num: 2
+  func_dir: test1/, test2/
+  func_ope: deploy, remove
---
+++ test1/sestest.py
@@ -0,0 +1,7 @@
+import boto3
+
+def lambda_handler(event, context):
+  sendEmail()
+
+def sendEmail():
+   print 1
